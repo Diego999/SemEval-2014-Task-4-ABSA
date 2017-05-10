35b8d920f82bd1dccd07131104926d6aeec55c4c
Diego999 <diegoantognini@gmail.com>
2017-05-10 17:12:50 +0200

    Add preprocessed data for Laptop

--- src/convert_from_conll_format_laptop.py
+++ src/convert_from_conll_format_laptop.py
@@ -54,7 +54,7 @@ with open(pred_file, 'r', encoding='utf-8') as fp:
 
             # Just to check if the mapping (conversion) is correct from truth to truth !
             # Should give a F1-score of 1.0
-            tag = truth
+            # tag = truth
 
             if tag == ASPECT_TAG:
                 aspects.append((token, int(fr), int(to)))
@@ -63,41 +63,43 @@ with open(pred_file, 'r', encoding='utf-8') as fp:
 
 with open(pred_file.replace('txt', 'xml'), 'w', encoding='utf-8') as fp:
     fp.write('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<sentences>\n')
-    id_sent = 0
-    xmldoc = minidom.parse(ref_file)
-    sentence_list = xmldoc.getElementsByTagName('sentence')
-    for sentence in sentence_list:
-        text = sentence.getElementsByTagName('text')[0].childNodes[0].nodeValue
-        id = sentence.attributes['id'].value
 
-        fp.write('\t<sentence id="' + str(id) + '">\n')
-        fp.write('\t\t<text>' + text + '</text>\n')
+    if len(preds) > 0:
+        id_sent = 0
+        xmldoc = minidom.parse(ref_file)
+        sentence_list = xmldoc.getElementsByTagName('sentence')
+        for sentence in sentence_list:
+            text = sentence.getElementsByTagName('text')[0].childNodes[0].nodeValue
+            id = sentence.attributes['id'].value
 
-        aspects = preds[id_sent][1]
-        if len(aspects) > 0:
-            fp.write('\t\t<aspectTerms>\n')
-            i = 0
-            current_aspect, current_from, current_to = '', 0, 0
-            while i < len(aspects):
-                asp, fr, to = aspects[i]
-                if current_aspect == '':
-                    current_aspect, current_from, current_to = asp, fr, to
-                elif fr - 1 == current_to:
-                    current_aspect += ' ' + asp
-                    current_to = to
-                else:
+            fp.write('\t<sentence id="' + str(id) + '">\n')
+            fp.write('\t\t<text>' + text + '</text>\n')
+
+            aspects = preds[id_sent][1]
+            if len(aspects) > 0:
+                fp.write('\t\t<aspectTerms>\n')
+                i = 0
+                current_aspect, current_from, current_to = '', 0, 0
+                while i < len(aspects):
+                    asp, fr, to = aspects[i]
+                    if current_aspect == '':
+                        current_aspect, current_from, current_to = asp, fr, to
+                    elif fr - 1 == current_to:
+                        current_aspect += ' ' + asp
+                        current_to = to
+                    else:
+                        current_aspect, current_from, current_to = map_aspect_to_text(text, current_aspect, current_from, current_to)
+                        fp.write('\t\t\t<aspectTerm term="' + replace(current_aspect) + '" polarity="" from="' + str(current_from) + '" to="' + str(current_to) + '"/>\n')
+                        current_aspect, current_from, current_to = asp, fr, to
+                    i += 1
+
+                if current_aspect != '':
                     current_aspect, current_from, current_to = map_aspect_to_text(text, current_aspect, current_from, current_to)
                     fp.write('\t\t\t<aspectTerm term="' + replace(current_aspect) + '" polarity="" from="' + str(current_from) + '" to="' + str(current_to) + '"/>\n')
-                    current_aspect, current_from, current_to = asp, fr, to
-                i += 1
+                    current_aspect, current_from, current_to = '', 0, 0
 
-            if current_aspect != '':
-                current_aspect, current_from, current_to = map_aspect_to_text(text, current_aspect, current_from, current_to)
-                fp.write('\t\t\t<aspectTerm term="' + replace(current_aspect) + '" polarity="" from="' + str(current_from) + '" to="' + str(current_to) + '"/>\n')
-                current_aspect, current_from, current_to = '', 0, 0
+                fp.write('\t\t</aspectTerms>\n')
+            fp.write('\t</sentence>\n')
 
-            fp.write('\t\t</aspectTerms>\n')
-        fp.write('\t</sentence>\n')
-
-        id_sent += 1
+            id_sent += 1
     fp.write('</sentences>\n')
