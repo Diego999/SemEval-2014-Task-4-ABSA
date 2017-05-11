e358e7b89a709b279f175c695a99ec8ed0fab215
Diego999 <diegoantognini@gmail.com>
2017-05-11 13:59:14 +0200

    Fix bug empty folder

--- gather_results.py
+++ gather_results.py
@@ -33,16 +33,17 @@ for d in dirs:
             except ValueError:
                 pass
 
-        epoch = str(int(files[-1][0:3]) - patience).rjust(3, '0')
-        for f in files:
-            if f.startswith(epoch) and f.endswith('aspect_evaluation.txt'):
-                type_set = f.split('_')[1].split('.')[0]
-                with open(join(path_dir, f), 'r', encoding='utf-8') as fp:
-                    lines = fp.readlines()
-                    prec, rec, f1 = lines[-1].rstrip().split(';')
-                    prec, rec = [float(t.split(' ')[-1][:-1]) for t in [prec, rec]]
-                    f1 = float(f1.split(':')[1].lstrip().split(' ')[0])
-                    final_res[type_dataset][type_set].append((str(int(epoch)), str(prec), str(rec), str(f1)))
+        if len(files) > 0:
+            epoch = str(int(files[-1][0:3]) - patience).rjust(3, '0')
+            for f in files:
+                if f.startswith(epoch) and f.endswith('aspect_evaluation.txt'):
+                    type_set = f.split('_')[1].split('.')[0]
+                    with open(join(path_dir, f), 'r', encoding='utf-8') as fp:
+                        lines = fp.readlines()
+                        prec, rec, f1 = lines[-1].rstrip().split(';')
+                        prec, rec = [float(t.split(' ')[-1][:-1]) for t in [prec, rec]]
+                        f1 = float(f1.split(':')[1].lstrip().split(' ')[0])
+                        final_res[type_dataset][type_set].append((str(int(epoch)), str(prec), str(rec), str(f1)))
 
 for d, t in final_res.items():
     for i in range(len(final_res[d]['train'])):
